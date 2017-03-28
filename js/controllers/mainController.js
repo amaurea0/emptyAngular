6c9b9d9e0ddb8564b4e145e0a27a36925c55a819
Amaurea <aurore.chatagnier@gmail.com>
2017-03-28 12:43:16 +0200

    form which add elements in a table below

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,4 +1,20 @@
 // MAIN CONTROLLER
 function mainController($scope) {
-    
+
+    $scope.users = [];
+    $scope.foo = function () {
+        var user = {};
+        user['nom'] = $scope.nom.trim();
+        user['prenom'] = $scope.prenom.trim();
+        user['mail'] = $scope.mail.trim();
+        $scope.nom = '';
+        $scope.prenom = '';
+        $scope.mail = '';
+        $scope.users.push(user);
+
+        alert(users);
+
+    };
 }
+
+
